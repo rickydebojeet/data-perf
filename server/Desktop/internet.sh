d6a72e90ce9f850cf02f9c9e40e1c3a8bef5c1bd
Debojeet Das <debojeetofficial@gmail.com>
2023-05-11 16:20:38 +0530

    final commit

--- server/Desktop/internet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/bin/curl -s --location-trusted -u 22d0371:224fd339dcb046c488173fdd55610977 "https://internet-sso.iitb.ac.in/login.php" | \
-grep -q window.location.href && \
-echo 'Logged in!' || \
-echo 'Something is wrong or already logged in!'
